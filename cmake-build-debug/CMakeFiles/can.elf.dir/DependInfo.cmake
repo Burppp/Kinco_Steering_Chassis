49e3424e9bb18e5d9b56e65e20289e53e73dbf4c
嗝嗝 <1483543906@qq.com>
2025-04-04 15:04:22 +0800

    init

---
+++ cmake-build-debug/CMakeFiles/can.elf.dir/DependInfo.cmake
@@ -0,0 +1,76 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "C:/Users/Lumos/Desktop/14.CAN/Startup/startup_stm32f407ighx.s" "C:/Users/Lumos/Desktop/14.CAN/cmake-build-debug/CMakeFiles/can.elf.dir/Startup/startup_stm32f407ighx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "ARM_MATH_CM4"
+  "ARM_MATH_MATRIX_CHECK"
+  "ARM_MATH_ROUNDING"
+  "DEBUG"
+  "STM32F407xx"
+  "USE_HAL_DRIVER"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "C:/Users/Lumos/Desktop/14.CAN/Inc"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Inc"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/CMSIS/Device/ST/STM32F4xx/Include"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/CMSIS/Include"
+  "C:/Users/Lumos/Desktop/14.CAN/bsp/boards"
+  "C:/Users/Lumos/Desktop/14.CAN/application"
+  "C:/Users/Lumos/Desktop/14.CAN/component/boards"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj" "gcc" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c.obj" "gcc" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_can.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj" "gcc" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_cortex.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj" "gcc" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_dma_ex.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj" "gcc" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_exti.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj" "gcc" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ex.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj" "gcc" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_flash_ramfunc.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj" "gcc" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_gpio.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj" "gcc" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_pwr_ex.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj" "gcc" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_rcc_ex.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj" "gcc" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/can.elf.dir/Drivers/STM32F4xx_HAL_Driver/Src/stm32f4xx_hal_tim_ex.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Src/can.c" "CMakeFiles/can.elf.dir/Src/can.c.obj" "gcc" "CMakeFiles/can.elf.dir/Src/can.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Src/gpio.c" "CMakeFiles/can.elf.dir/Src/gpio.c.obj" "gcc" "CMakeFiles/can.elf.dir/Src/gpio.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Src/main.c" "CMakeFiles/can.elf.dir/Src/main.c.obj" "gcc" "CMakeFiles/can.elf.dir/Src/main.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Src/stm32f4xx_hal_msp.c" "CMakeFiles/can.elf.dir/Src/stm32f4xx_hal_msp.c.obj" "gcc" "CMakeFiles/can.elf.dir/Src/stm32f4xx_hal_msp.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Src/stm32f4xx_it.c" "CMakeFiles/can.elf.dir/Src/stm32f4xx_it.c.obj" "gcc" "CMakeFiles/can.elf.dir/Src/stm32f4xx_it.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Src/syscalls.c" "CMakeFiles/can.elf.dir/Src/syscalls.c.obj" "gcc" "CMakeFiles/can.elf.dir/Src/syscalls.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Src/sysmem.c" "CMakeFiles/can.elf.dir/Src/sysmem.c.obj" "gcc" "CMakeFiles/can.elf.dir/Src/sysmem.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/Src/system_stm32f4xx.c" "CMakeFiles/can.elf.dir/Src/system_stm32f4xx.c.obj" "gcc" "CMakeFiles/can.elf.dir/Src/system_stm32f4xx.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/application/CAN_receive.c" "CMakeFiles/can.elf.dir/application/CAN_receive.c.obj" "gcc" "CMakeFiles/can.elf.dir/application/CAN_receive.c.obj.d"
+  "C:/Users/Lumos/Desktop/14.CAN/bsp/boards/bsp_can.c" "CMakeFiles/can.elf.dir/bsp/boards/bsp_can.c.obj" "gcc" "CMakeFiles/can.elf.dir/bsp/boards/bsp_can.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
